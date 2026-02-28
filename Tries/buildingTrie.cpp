08987124f56b37cebf4dbe916cdac1a0237a74a3
Devaki Patil <devakipatil7@gmail.com>
2026-02-28 23:14:12 +0530

    Created folder Trie

---
+++ Tries/buildingTrie.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Node {
+public:
+    unordered_map<char, Node*> children;
+    bool endOfWord;
+
+    Node() {
+        endOfWord = false;
+    }
+};
+
+class Trie {
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string key) { //O(L)
+        Node* temp = root;
+
+        for(int i=0; i<key.size(); i++) {
+            if(temp->children.count(key[i]) == 0) { //check if key[i] already exists
+                temp->children[key[i]] = new Node(); //insert newNode
+            }
+            temp = temp->children[key[i]];
+        }
+
+        temp->endOfWord = true;
+    }
+
+    bool search(string key) { //O(L)
+        Node* temp = root;
+
+        for(int i=0; i<key.size(); i++) {
+            if(temp->children.count(key[i])) {
+                temp = temp->children[key[i]];
+            } else {
+                return false;
+            }
+        }
+
+        return (temp->endOfWord);
+    }
+};
+
+int main() {
+    vector<string> words = {"the", "a", "there", "their", "any", "thee"};
+    Trie trie;
+
+    for(string word: words) {
+        trie.insert(word);
+    }
+
+    cout << trie.search("their") << endl;
+
+    return 0;
+}
