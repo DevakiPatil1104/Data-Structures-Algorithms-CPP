603710ea0a84d26c6f6de8dad445eb4daa447476
Devaki Patil <devakipatil7@gmail.com>
2026-02-28 14:02:03 +0530

    Hashing folder updated

---
+++ Hashing/itineraryTickets.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+void printItinerary(unordered_map<string, string> tickets) {
+    // find starting point
+    unordered_set<string> to;
+
+    for(pair<string, string> ticket: tickets) { //(from, to)
+        to.insert(ticket.second);
+    }
+
+    string start = "";
+    for(pair<string, string> ticket: tickets) {
+        if(to.find(ticket.first) == to.end()) {
+            start = ticket.first;
+        }
+    }
+
+    // print plan
+    cout << "start -> " << start << " -> ";
+    while(tickets.count(start)) {
+        cout << tickets[start] << " -> ";
+        start = tickets[start];
+    }
+    cout << "destination\n";
+}
+
+int main() {
+    unordered_map<string, string> tickets;
+    tickets["Chennai"] = "Bengaluru";
+    tickets["Mumbai"] = "Delhi";
+    tickets["Goa"] = "Chennai";
+    tickets["Delhi"] = "Goa";
+
+    printItinerary(tickets);
+
+    return 0;
+}
