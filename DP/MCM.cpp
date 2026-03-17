201a603ca17d39f68d874a442024df0aa93e07d7
Devaki Patil <devakipatil7@gmail.com>
2026-03-18 00:11:56 +0530

    Updated MCM problem in DP

--- DP/MCM.cpp
+++ DP/MCM.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int mcmRec(vector<int> arr, int i, int j) {
+int mcmRec(vector<int> arr, int i, int j) {  //O(2^n)
     if(i == j) return 0; //BC
 
     int ans = INT_MAX;
@@ -20,11 +20,36 @@ int mcmRec(vector<int> arr, int i, int j) {
     return ans;
 }
 
+int mcmMemo(vector<int> arr, int i, int j, vector<vector<int>>& dp) {  //O(n^3)
+    if(i == j) return 0; //BC
+
+    if(dp[i][j] != -1) {
+        return dp[i][j];
+    } 
+
+    int ans = INT_MAX;
+
+    for(int k=i; k<j; k++) {
+        // set1 => i to k
+        int cost1 = mcmMemo(arr, i, k, dp);
+
+        // set2 => k+1 to j
+        int cost2 = mcmMemo(arr, k+1, j, dp);
+
+        int currCost = cost1 + cost2 + (arr[i-1] * arr[k] * arr[j]);
+        ans = min(ans, currCost);
+    }
+
+    return dp[i][j] = ans;;
+}
+
 int main() {
     vector<int> arr = {1, 2, 3, 4, 3};
     int n = arr.size();
 
     cout << mcmRec(arr, 1, n-1) << endl;
-    
+
+    vector<vector<int>> dp(n, vector<int>(n, -1));
+    cout << mcmMemo(arr, 1, n-1, dp) << endl;
     return 0;
 }
