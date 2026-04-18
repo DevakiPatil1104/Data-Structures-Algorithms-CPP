a468bf68af805f890a98e5f4057d4e088e990f4f
Devaki Patil <devakipatil7@gmail.com>
2026-04-19 01:05:53 +0530

    Added Bellman Ford's Algo to Graphs

--- Graph/bellmanFordAlgo.cpp
+++ Graph/bellmanFordAlgo.cpp
@@ -17,11 +17,28 @@ public:
     }
 };
 
+void bellmanFord(int V, vector<vector<Edge>> graph, int src) {
+    vector<int> dist(V, INT16_MAX);
+    dist[src] = 0;
 
+    for(int i=0; i<V-1; i++) { //traversing on every edge
+        for(int u=0; u<V; u++) {
+            for(Edge e : graph[u]) {
+                if(dist[e.v] > dist[u] + e.wt) {
+                    dist[e.v] = dist[u] + e.wt;
+                }
+            }
+        }
+    }
 
+    for(int i=0; i<V; i++) {
+        cout << dist[i] << " ";
+    }
+    cout << endl;
+}
 
 int main(){
-    int V = 6;
+    int V = 5;
     // Bellman Ford Graph
     vector<vector<Edge>> graph(V);
 
@@ -30,13 +47,13 @@ int main(){
 
     graph[1].push_back(Edge(2, -4));
 
-    graph[4].push_back(Edge(3, 2));
+    graph[2].push_back(Edge(3, 2));
 
-    graph[2].push_back(Edge(4, 4));
+    graph[3].push_back(Edge(4, 4));
 
-    graph[3].push_back(Edge(1, -1));
+    graph[4].push_back(Edge(1, -1));
 
-    
+    bellmanFord(V, graph, 0);
 
     return 0;
 }
