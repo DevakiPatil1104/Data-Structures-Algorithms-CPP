af40748c0da05f46b7533fd24363ad128c8df9f0
Devaki Patil <devakipatil7@gmail.com>
2026-04-18 23:20:28 +0530

    Added dijkstra algorithm to Graphs

--- Graph/dijkstraAlgo.cpp
+++ Graph/dijkstraAlgo.cpp
@@ -17,6 +17,32 @@ public:
     }
 };
 
+void dijkstra(int src, vector<vector<Edge>> graph, int V) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; //min heap
+    // pair(dist[v], v)
+    vector<int> dist(V, INT16_MAX);
+    pq.push(make_pair(0, src));
+    dist[src] = 0;
+
+    while(pq.size() > 0) {
+        int u = pq.top().second; 
+        pq.pop();
+
+        vector<Edge> edges = graph[u];
+        for(Edge e : edges) { //e.v, e.wt
+            if(dist[e.v] > dist[u] + e.wt) {
+                dist[e.v] = dist[u] + e.wt;
+                pq.push(make_pair(dist[e.v], e.v));
+            }
+        }
+    }
+
+    for(int d : dist) {
+        cout << d << " ";
+    } 
+    cout << endl;
+}
+
 int main(){
     int V = 6;
     vector<vector<Edge>> graph(V);
@@ -32,7 +58,9 @@ int main(){
     graph[3].push_back(Edge(5, 1));
 
     graph[4].push_back(Edge(3, 2));
-    graph[0].push_back(Edge(5, 5));
+    graph[0].push_back(Edge(2, 5));
+
+    dijkstra(0, graph, V);
 
     return 0;
 }
